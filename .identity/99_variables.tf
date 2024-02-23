65f38d13b775792516670a565de435735112e7cc
Jacopo <jacopo.carlini@gmail.com>
2024-02-23 16:22:54 +0100

    [VAS-760] todo resolved

--- .identity/99_variables.tf
+++ .identity/99_variables.tf
@@ -1,11 +1,11 @@
 locals {
   github = {
     org        = "pagopa"
-    repository = "TODO" #TODO
+    repository = "pagopa-backoffice-external"
   }
 
   prefix         = "pagopa"
-  domain         = "TODO" #TODO
+  domain         = "selfcare"
   location_short = "weu"
   product        = "${var.prefix}-${var.env_short}"
 
