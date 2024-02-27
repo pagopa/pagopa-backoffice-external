3e20ae09aae14cbfa6909e8748cd9b20ba07079f
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-02-27 11:43:26 +0100

    [VAS-515] feat: Add api to export broker institutions with paged filter

---
+++ src/main/java/it/pagopa/selfcare/pagopa/repository/BrokerInstitutionsRepository.java
@@ -0,0 +1,16 @@
+package it.pagopa.selfcare.pagopa.repository;
+
+import it.pagopa.selfcare.pagopa.entities.BrokerInstitutionsEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BrokerInstitutionsRepository extends MongoRepository<BrokerInstitutionsEntity, String> {
+
+    @Query(value = "{ brokerCode : ?0 }", fields = "{messages: {institutions: [?1, ?2]}}")
+    List<BrokerInstitutionsEntity> findPagedInstitutionsByBrokerCode(String brokerCode, int skip, Integer limit);
+
+}
