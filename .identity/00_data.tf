06f23a2aa3380c6f2784e2654db3029675e1156b
Jacopo <jacopo.carlini@gmail.com>
2024-02-29 15:29:17 +0100

    full name

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -1,25 +1,13 @@
-data "azurerm_storage_account" "tf_storage_account"{
-  name                = "pagopainfraterraform${var.env}"
-  resource_group_name = "io-infra-rg"
-}
-
-data "azurerm_resource_group" "dashboards" {
-  name = "dashboards"
-}
-
-data "azurerm_kubernetes_cluster" "aks" {
-  name                = local.aks_cluster.name
-  resource_group_name = local.aks_cluster.resource_group_name
-}
-
-data "github_organization_teams" "all" {
-  root_teams_only = true
-  summary_only    = true
-}
 
 data "azurerm_user_assigned_identity" "identity_cd_01"{
   name = "${local.prefix}-${var.env_short}-${local.domain}-01-github-cd-identity"
   resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+}
+
+
+data "github_organization_teams" "all" {
+  root_teams_only = true
+  summary_only    = true
 }
 
 data "azurerm_key_vault" "key_vault" {
@@ -30,10 +18,6 @@ data "azurerm_key_vault" "key_vault" {
 data "azurerm_key_vault" "domain_key_vault" {
   name                = "pagopa-${var.env_short}-${local.domain}-kv"
   resource_group_name = "pagopa-${var.env_short}-${local.domain}-sec-rg"
-}
-
-data "azurerm_resource_group" "apim_resource_group" {
-  name = "${local.product}-api-rg"
 }
 
 data "azurerm_key_vault_secret" "key_vault_sonar" {
