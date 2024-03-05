63ee047b855d11329c979ce42d198dbbf7ccbede
giomella <gioele.mella@emeal.nttdata.com>
2024-03-05 11:59:36 +0100

    [VAS-501] added unit tests

--- src/main/java/it/pagopa/selfcare/pagopa/model/tavoloop/OperativeTableResourceList.java
+++ src/main/java/it/pagopa/selfcare/pagopa/model/tavoloop/OperativeTableResourceList.java
@@ -2,13 +2,17 @@ package it.pagopa.selfcare.pagopa.model.tavoloop;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OperativeTableResourceList {
 
     @ApiModelProperty(value = "All Tavolo operativo details", required = true)
