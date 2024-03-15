a2e9de66ec64d807019b03c886c39de9df560873
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-03-15 10:53:54 +0100

    [VAS-853] feat: updated openapis

--- infra/99_locals.tf
+++ infra/99_locals.tf
@@ -4,7 +4,8 @@ locals {
   apim = {
     name                   = "${local.product}-apim"
     rg                     = "${local.product}-api-rg"
-    bo_external_product_id = "selfcare-bo-external"
+    bo_external_product_psp_id = "selfcare-bo-external-psp"
+    bo_external_product_ec_id = "selfcare-bo-external-ec"
     bo_helpdesk_product_id = "selfcare-bo-helpdesk"
   }
 }
