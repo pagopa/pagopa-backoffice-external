ff0c59951576eb20d1a0134f26157c7aa11123d0
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-09-22 18:27:25 +0200

    [PIDM-368] feat: Add first impl for GET CI with cbill

--- src/main/java/it/pagopa/selfcare/pagopa/repository/BrokerInstitutionsRepository.java
+++ src/main/java/it/pagopa/selfcare/pagopa/repository/BrokerInstitutionsRepository.java
@@ -2,7 +2,6 @@ package it.pagopa.selfcare.pagopa.repository;
 
 import it.pagopa.selfcare.pagopa.entities.BrokerInstitutionAggregate;
 import it.pagopa.selfcare.pagopa.entities.BrokerInstitutionsEntity;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
