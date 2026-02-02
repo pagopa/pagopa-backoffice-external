03f54578d3a8f70d01afd4c491e6afb13a0a32bf
Stefano Rondinella <stefrondinella@live.it>
2026-02-02 16:39:43 +0100

    refactor(tests): remove sonarQ issues

--- src/test/java/it/pagopa/selfcare/pagopa/controller/HomeControllerTest.java
+++ src/test/java/it/pagopa/selfcare/pagopa/controller/HomeControllerTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HomeControllerTest {
+class HomeControllerTest {
 
     @Autowired
     private MockMvc mvc;
