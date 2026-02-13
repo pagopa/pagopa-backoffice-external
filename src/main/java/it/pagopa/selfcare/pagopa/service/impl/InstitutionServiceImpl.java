52ea485bd392202a52093d00300d17b08286000b
Stefano Rondinella <stefrondinella@live.it>
2026-02-13 15:46:19 +0100

    refactor: remove sonarQube issues

--- src/main/java/it/pagopa/selfcare/pagopa/service/impl/InstitutionServiceImpl.java
+++ src/main/java/it/pagopa/selfcare/pagopa/service/impl/InstitutionServiceImpl.java
@@ -1,6 +1,5 @@
 package it.pagopa.selfcare.pagopa.service.impl;
 
-import it.pagopa.selfcare.pagopa.entities.InstitutionConsentEntity;
 import it.pagopa.selfcare.pagopa.exception.AppError;
 import it.pagopa.selfcare.pagopa.exception.AppException;
 import it.pagopa.selfcare.pagopa.model.PageInfo;
@@ -8,16 +7,13 @@ import it.pagopa.selfcare.pagopa.model.institutions.services.*;
 import it.pagopa.selfcare.pagopa.repository.InstitutionServiceRtpConsentRepository;
 import it.pagopa.selfcare.pagopa.service.InstitutionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +24,7 @@ public class InstitutionServiceImpl implements InstitutionService {
     private final InstitutionServiceRtpConsentRepository repository;
 
     @Autowired
-    public InstitutionServiceImpl(InstitutionServiceRtpConsentRepository repository){
+    public InstitutionServiceImpl(InstitutionServiceRtpConsentRepository repository) {
         this.repository = repository;
     }
 
@@ -38,60 +34,58 @@ public class InstitutionServiceImpl implements InstitutionService {
      * @param institutionsServiceFilter
      * @return
      */
-    public InstitutionsServicesConsentResponse getInstitutionServiceConsentFilteredByDatesAndByConsent(InstitutionsServiceFilter institutionsServiceFilter){
+    public InstitutionsServicesConsentResponse getInstitutionServiceConsentFilteredByDatesAndByConsent(InstitutionsServiceFilter institutionsServiceFilter) {
 
         List<InstitutionServiceConsent> institutionServiceConsentList;
-        Criteria criteria = new Criteria();
         Pageable pageable = PageRequest.of(institutionsServiceFilter.getPage(), institutionsServiceFilter.getPageSize(), Sort.Direction.DESC, "consentDate");
-        Page<InstitutionConsentEntity> page;
-        Query query = new Query();
         long count = 0;
 
-        switch(institutionsServiceFilter.getServiceId()){
+        switch (institutionsServiceFilter.getServiceId()) {
             case RTP:
 
                 OffsetDateTime startDate = Optional.ofNullable(institutionsServiceFilter.getStartingData()).orElse(OffsetDateTime.ofInstant(Instant.EPOCH, ZoneOffset.UTC));
                 OffsetDateTime endDate = institutionsServiceFilter.getEndingDate();
                 Consent consent = institutionsServiceFilter.getConsent();
 
-                count = repository.countByDateAndConsent(startDate,endDate,consent);
+                count = repository.countByDateAndConsent(startDate, endDate, consent);
 
                 institutionServiceConsentList = repository
-                        .findByDateAndConsent(startDate,endDate,consent,pageable)
+                        .findByDateAndConsent(startDate, endDate, consent, pageable)
                         .stream()
                         .map(institutionConsentEntity ->
-                            InstitutionServiceConsent.builder()
-                                    .institutionInfo(
-                                            InstitutionInfo
-                                                    .builder()
-                                                    .taxCode(institutionConsentEntity.getInstitutionTaxCode())
-                                                    .name(institutionConsentEntity.getName())
-                                                    .build()
-                                    )
-                                    .consentInfo(
-                                            ConsentInfo
-                                                    .builder()
-                                                    .consent(institutionConsentEntity.getConsent())
-                                                    .date(institutionConsentEntity.getConsentDate().atOffset(ZoneOffset.UTC))
-                                                    .build()
-                                    )
-                                    .build()
+                                InstitutionServiceConsent.builder()
+                                        .institutionInfo(
+                                                InstitutionInfo
+                                                        .builder()
+                                                        .taxCode(institutionConsentEntity.getInstitutionTaxCode())
+                                                        .name(institutionConsentEntity.getName())
+                                                        .build()
+                                        )
+                                        .consentInfo(
+                                                ConsentInfo
+                                                        .builder()
+                                                        .consent(institutionConsentEntity.getConsent())
+                                                        .date(institutionConsentEntity.getConsentDate().atOffset(ZoneOffset.UTC))
+                                                        .build()
+                                        )
+                                        .build()
                         )
                         .toList();
                 break;
-            default: throw new AppException(AppError.SERVICE_NOT_HANDLED);
+            default:
+                throw new AppException(AppError.SERVICE_NOT_HANDLED);
         }
 
-        long totalPages = count/institutionsServiceFilter.getPageSize();
+        long totalPages = count / institutionsServiceFilter.getPageSize();
 
-        if((totalPages % institutionsServiceFilter.getPageSize()) != 0) {
-          totalPages++;
+        if ((totalPages % institutionsServiceFilter.getPageSize()) != 0) {
+            totalPages++;
         }
 
-        return  InstitutionsServicesConsentResponse
+        return InstitutionsServicesConsentResponse
                 .builder()
                 .results(
-                    institutionServiceConsentList
+                        institutionServiceConsentList
                 )
                 .pageInfo(
                         PageInfo.builder()
@@ -103,7 +97,6 @@ public class InstitutionServiceImpl implements InstitutionService {
                 ).build();
 
     }
-
 
 
 }
--- src/test/java/it/pagopa/selfcare/pagopa/service/impl/InstitutionServicesImplTest.java
+++ src/test/java/it/pagopa/selfcare/pagopa/service/impl/InstitutionServicesImplTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
